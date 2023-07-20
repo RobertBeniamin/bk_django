7f7bb6dbd959dc7d56adadd442e6d10ea203e4a9
RobertBeniamin <robertbeniamin89@yahoo.com>
2023-07-20 20:28:21 +0300

    primult commit

---
+++ prim_contact/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+pagina_contacte = """
+<h1>Acesta este un contact FICTIV</h1>
+<p>Nume: Ion</p>
+<p>Prenume: Ionescu</p>
+<p>Telefon: 07777777</p>
+<p>Email: blabla@mm.com</p>
+"""
